775a00f0c05d26db52ecc1dd43a662639f13d0a5
Sean Remenyi <sean.remenyi@gmail.com>
2021-06-14 19:17:04 +1000

    creates new csv file or words still to learn

--- main.py
+++ main.py
@@ -3,10 +3,16 @@ import pandas
 import random
 
 BACKGROUND_COLOR = "#B1DDC6"
-
-data = pandas.read_csv("data/french_words.csv")
-to_learn = data.to_dict(orient="records")
 current_card = {}
+to_learn = {}
+try:
+    data = pandas.read_csv("data/words_to_learn.csv")
+except FileNotFoundError:
+    original_data = pandas.read_csv("data/french_words.csv")
+    to_learn = original_data.to_dict(orient="records")
+else:
+    to_learn = data.to_dict(orient="records")
+
 
 
 def next_card():
@@ -18,10 +24,19 @@ def next_card():
     canvas.itemconfig(card_background, image=card_front_img)
     flip_timer = window.after(3000, func=flip_card)
 
+
 def flip_card():
     canvas.itemconfig(card_title, text="English", fill="white")
     canvas.itemconfig(card_word, text=current_card["English"], fill="white")
     canvas.itemconfig(card_background, image=card_back_img)
+
+
+def is_known():
+    to_learn.remove(current_card)
+    data = pandas.DataFrame(to_learn)
+    data.to_csv("data/words_to_learn", index=False)
+    next_card()
+
 
 window = Tk()
 window.title("Learn French")
@@ -42,10 +57,10 @@ canvas.grid(row=0, column=0, columnspan=2)
 
 
 cross_img = PhotoImage(file="images/wrong.png")
-unknown_button = Button(image=cross_img, highlightthickness=0, command= next_card)
+unknown_button = Button(image=cross_img, highlightthickness=0, command=next_card)
 unknown_button.grid(row=1, column=0)
 check_img = PhotoImage(file="images/right.png")
-correct_button = Button(image=check_img, highlightthickness=0, command= next_card)
+correct_button = Button(image=check_img, highlightthickness=0, command=is_known)
 correct_button.grid(row=1, column=1)
 
 
